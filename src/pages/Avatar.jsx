9aaeec9e084dd0f8016cebca8e3db8460e7ae3fa
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-16 22:44:10 +0530

    testing mobile landscape

--- src/pages/Avatar.jsx
+++ src/pages/Avatar.jsx
@@ -201,6 +201,56 @@ const AvatarWrapper = styled.div`
     background-color: green;
   }
 
+  /* for big-tablets & mobile-landscape */
+  @media screen and (max-width: 991px) {
+    background-color: orange;
+    gap: 5vw;
+    margin-top: 5vw;
+    h1 {
+      font-size: 7vw;
+    }
+    main {
+      gap: 6vw;
+      .single-image {
+        border: 0.6vw solid #212121;
+        height: 50vw;
+        width: 40vw;
+      }
+      .selected {
+        border: 0.6vw solid #f1f1f1;
+        .selected-text {
+          bottom: 6vw;
+          right: 0.7vw;
+          font-size: 3.3vw;
+          padding: 1vw 1.7vw;
+          border: 0.4vw solid #f1f1f1;
+        }
+      }
+      .img-hover-effect {
+        gap: 3vw;
+        img {
+          border-radius: 50%;
+          width: 12vw;
+          height: 12vw;
+          border: 0.2vw solid #f1f1f1;
+        }
+        p {
+          padding: 1vw;
+          /* line-height: 2vh; */
+          font-size: 3vw;
+        }
+      }
+    }
+    button {
+      box-shadow: 0 0 1vw rgba(33, 33, 33, 0.7);
+      width: 86vw;
+      height: 8vh;
+      font-size: 6vw;
+      padding: 1.7vw 2vw;
+      border-radius: 6vw;
+    }
+  }
+
   /* for phones */
   @media screen and (max-width: 767px) {
     background-color: pink;
